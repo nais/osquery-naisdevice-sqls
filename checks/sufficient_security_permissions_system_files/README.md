25c7b1b37c9e2ff067b5b528f689fff311bac3d3
Christian Chavez <christian.chavez@nav.no>
2021-01-22 12:25:27 +0100

    Refactor(security_permissions_system_files): Merge suggestion from Kolide.
    Ref. https://nav-it.slack.com/archives/C010NG02FEU/p1611252702000400?thread_ts=1611068619.007900&cid=C010NG02FEU,
    1. I split out the "files should not exist" into their own check
    2. I ensured no files were forgotten about

--- checks/sufficient_security_permissions_system_files/README.md
+++ checks/sufficient_security_permissions_system_files/README.md
@@ -1,4 +1,2 @@
 To test:
-`osqueryi < <(cat expected_files.sql list_all_files.sql)`
-`osqueryi < <(cat expected_files.sql list_violating_files.sql)`
-`osqueryi < absent_files.sql`
+`osqueryi < list_violating_files.sql`
--- checks/sufficient_security_permissions_system_files/list_violating_files.sql
+++ checks/sufficient_security_permissions_system_files/list_violating_files.sql
@@ -1,17 +1,73 @@
-SELECT
-  e.path,
-  u.username,
-  g.groupname,
-  e.recommended_mode,
-  e.cis_recommendation,
-  case when regex_match(f.mode, e.mode, 0) != "" then 0 else 1 end AS mode_match
-FROM
-  expected_files e
-  JOIN file f USING(path)
-  JOIN users u ON e.uid = u.uid
-  JOIN groups g ON e.gid = g.gid
-WHERE
-  f.uid != e.uid OR
-  f.gid != e.gid OR
-  mode_match
-;
+WITH
+cis_perms (cis_id,path,mode_req,uid_req,gid_req_1,gid_req_2) AS (VALUES
+('1.5.1','/boot/grub/grub.cfg','0400','0','root',''),
+('1.8.1.4','/etc/motd','0644','0','root',''),
+('1.8.1.5','/etc/issue','0644','0','root',''),
+('1.8.1.6','/etc/issue.net','0644','0','root',''),
+('3.3.2','/etc/hosts.allow','0644','0','root',''),
+('3.3.3','/etc/hosts.deny','0644','0','root',''),
+('5.1.2','/etc/crontab','0600','0','root',''),
+('5.1.3','/etc/cron.hourly','0700','0','root',''),
+('5.1.4','/etc/cron.daily','0700','0','root',''),
+('5.1.5','/etc/cron.weekly','0700','0','root',''),
+('5.1.6','/etc/cron.monthly','0700','0','root',''),
+('5.1.7','/etc/cron.d','0700','0','root',''),
+('5.1.8','/etc/cron.allow','0640','0','root',''),
+('5.1.8','/etc/at.allow','0640','0','root',''),
+('6.1.2','/etc/passwd','0644','0','root',''),
+('6.1.3','/etc/gshadow-','0640','0','root','shadow'),
+('6.1.4','/etc/shadow','0640','0','root','shadow'),
+('6.1.5','/etc/group','0644','0','root',''),
+('6.1.6','/etc/passwd-','0600','0','root',''),
+('6.1.7','/etc/shadow-','0640','0','root','shadow'),
+('6.1.8','/etc/group-','0644','0','root',''),
+('6.1.9','/etc/gshadow','0640','0','root','shadow')
+),
+groups_temp AS (select groupname, gid from  groups where groupname IN (select distinct gid_req_1 from cis_perms UNION select distinct gid_req_2 from cis_perms)),
+merge_data AS (
+  SELECT cis.*, f.mode, f.uid, f.gid, f.mtime, f.type FROM cis_perms cis LEFT JOIN file f USING(path)),
+mode_split AS (
+  SELECT *,
+  SUBSTR(mode, -3, 1) AS pos_u,
+  SUBSTR(mode, -2, 1) AS pos_g,
+  SUBSTR(mode, -1, 1) AS pos_w,
+  SUBSTR(mode_req, -3, 1) AS pos_u_req,
+  SUBSTR(mode_req, -2, 1) AS pos_g_req,
+  SUBSTR(mode_req, -1, 1) AS pos_w_req
+  FROM merge_data WHERE mode != mode_req OR uid != uid_req OR gid NOT IN (gid_req_1,gid_req_2)),
+bit_split AS (
+SELECT *,
+  (pos_u >> 1) & 1 AS u_r,
+  (pos_u >> 2) & 1 AS u_w,
+  (pos_u >> 3) & 1 AS u_x,
+  (pos_g >> 1) & 1 AS g_r,
+  (pos_g >> 2) & 1 AS g_w,
+  (pos_g >> 3) & 1 AS g_x,
+  (pos_w >> 1) & 1 AS w_r,
+  (pos_w >> 2) & 1 AS w_w,
+  (pos_w >> 3) & 1 AS w_x,
+  (pos_u_req >> 1) & 1 AS u_r_req,
+  (pos_u_req >> 2) & 1 AS u_w_req,
+  (pos_u_req >> 3) & 1 AS u_x_req,
+  (pos_g_req >> 1) & 1 AS g_r_req,
+  (pos_g_req >> 2) & 1 AS g_w_req,
+  (pos_g_req >> 3) & 1 AS g_x_req,
+  (pos_w_req >> 1) & 1 AS w_r_req,
+  (pos_w_req >> 2) & 1 AS w_w_req,
+  (pos_w_req >> 3) & 1 AS w_x_req
+ FROM mode_split
+ JOIN groups_temp USING(gid)),
+excessive_permissions AS (
+  SELECT * FROM bit_split
+  WHERE u_r > u_r_req
+    OR u_w > u_w_req
+    OR u_x > u_x_req
+    OR g_r > g_r_req
+    OR g_w > g_w_req
+    OR g_x > g_x_req
+    OR w_r > w_r_req
+    OR w_w > w_w_req
+    OR w_x > w_x_req
+    OR groupname NOT IN (gid_req_1,gid_req_2)
+    OR uid != uid_req)
+SELECT path, mtime, mode, mode_req, uid, uid_req, gid, groupname, gid_req_1, gid_req_2, cis_id FROM excessive_permissions;
