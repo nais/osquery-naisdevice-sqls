1988ca68ef89c40fd0dc4558b92547f3c530fd78
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-01-28 13:48:08 +0100

    Correctly calculate time until xscreensaver lock

--- checks/lockscreen_timeout_activated_xscreensaver/xscreensaver_will_lock_on_timeout.sql
+++ checks/lockscreen_timeout_activated_xscreensaver/xscreensaver_will_lock_on_timeout.sql
@@ -3,7 +3,10 @@ WITH xscreensaver_config AS (
         MAX(CASE WHEN key = 'lock' THEN value END) as lock_enabled,
         MAX(CASE WHEN key = 'lockTimeout' THEN
                 (CASE WHEN length(value) < 8 THEN "0" || value ELSE value END)
-            END) as lock_timeout
+            END) as lock_timeout,
+        MAX(CASE WHEN key = 'timeout' THEN
+                (CASE WHEN length(value) < 8 THEN "0" || value ELSE value END)
+            END) as blank_timeout
     FROM
         kolide_ini
     WHERE
@@ -12,11 +15,14 @@ WITH xscreensaver_config AS (
 
 SELECT
     lock_enabled,
-    lock_timeout
+    lock_timeout,
+    strftime("%s", lock_timeout) - strftime("%s", "00:00:00") AS lock_timeout_seconds,
+    blank_timeout,
+    strftime("%s", blank_timeout) - strftime("%s", "00:00:00") AS blank_timeout_seconds
 FROM
     xscreensaver_config
 WHERE
     lock_enabled != 'true'
 OR
-    time(lock_timeout) > time("00:10:00")
+    lock_timeout_seconds + blank_timeout_seconds > 600
 ;
