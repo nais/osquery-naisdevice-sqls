7bd0df431a2f437247e46db4e7ff154faea2161a
Christian Chavez <christian.chavez@nav.no>
2021-01-29 13:49:05 +0100

    Refactor(unattended-upgrades): Remove dependency on osquery version 4.6+

--- checks/ensure_unattended_upgrades_installed/is_not_running.sql
+++ checks/ensure_unattended_upgrades_installed/is_not_running.sql
@@ -3,10 +3,11 @@ WITH unattended_startup_items  AS (
 		count(*) as item_count
 	FROM startup_items
 	WHERE
-		name LIKE 'unattended-upgrades%'
-	AND (
+		name LIKE 'unattended-upgrades'
+	AND
 		status = 'enabled'
-		OR status = 'active'
-	)
+	AND
+		path = '/etc/init.d/unattended-upgrades'
+
 )
-SELECT * FROM unattended_startup_items where item_count != 2;
+SELECT * FROM unattended_startup_items where item_count != 1;
