31134d1fe686d55f21fd5701aaa2c7a669b8fd43
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-05-25 15:36:15 +0200

    remote-access: ignore !main network ns & localhost

--- checks/no_remote_access/is_running.sql
+++ checks/no_remote_access/is_running.sql
@@ -1,3 +1,9 @@
+WITH main_network_namespace AS (
+	SELECT net_namespace
+	FROM process_namespaces
+	WHERE pid = 1
+)
+
 SELECT
 	t.alias as protocol,
 	l.port,
@@ -8,14 +14,13 @@ SELECT
 FROM
 	listening_ports l,
 	processes p,
-	etc_protocols t 
+	etc_protocols t
 WHERE
 	l.pid = p.pid 
 	AND (
-		(
-			l.port IN (5800, 5900)
-			AND l.address NOT IN ('127.0.0.1', '::1')
-		)
+		l.net_namespace IN (SELECT * FROM main_network_namespace)
+		AND l.address NOT IN ('127.0.0.1', '::1')
+		AND l.port IN (5800, 5900)
 		OR p.name IN (
 			'directvnc'
 			,'krfb'
