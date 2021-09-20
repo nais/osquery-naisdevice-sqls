895b491ca9c4dd3f4c622836da7853d1c1dc9cd0
Kent D <kent.daleng@nav.no>
2021-09-20 14:01:13 +0200

    wip: flesh out sql (it's kind of messy atm)
    Co-authored-by: x10an14 <christian.chavez@nav.no>

--- checks/sudo_logfile_set/sudo_logfile_set.sql
+++ checks/sudo_logfile_set/sudo_logfile_set.sql
@@ -2,29 +2,48 @@ WITH
 	log_files
 AS (
 	SELECT
-		COUNT(*) AS secured_sudo_logfile
+		COUNT(*) as secured_sudo_logfile
 	FROM file
-	WHERE 
-		path = '/var/log/auth.log'
+	WHERE
+		path == (SELECT regex_match(rule_details, '^logfile="(.+)"$', 1) as details from sudoers where details IS NOT NULL)
 	AND
-		uid == 0
+		uid == (SELECT uid FROM users WHERE username == 'syslog')
 	AND
-		gid == 0
-), sudoers_settings
+		gid == (SELECT gid FROM groups WHERE groupname == 'adm')
+	AND 
+		mode == '0640'
+), log_outputs
 AS (
 	SELECT
-		COUNT(*) as sudoers_logfile
-	from sudoers
+		COUNT(*) AS log_output_counts
+	FROM sudoers
 	WHERE
 		header == 'Defaults'
 	AND
-		rule_details == 'logfile="/var/log/auth.log"'
+		rule_details == 'log_output'
+), log_output_restrictions
+AS (
+	SELECT
+		COUNT(DISTINCT regex_match(rule_details, '.+bin/sudoreplay .*!log_output', 0)) AS allowlisted_counts
+	FROM sudoers
+), ensure_no_logging_disabled
+AS (
+	SELECT 
+		COUNT(DISTINCT rule_details) AS log_disabled_counts
+	FROM sudoers
+	WHERE rule_details like "%!log%"
 )
 SELECT
 	*
 FROM
-	log_files, sudoers_settings
+	log_files, log_outputs, log_output_restrictions, ensure_no_logging_disabled
 WHERE
-	sudoers_logfile != 1
-AND
-	secured_sudo_logfile != 1;
+	NOT (
+		secured_sudo_logfile == 1
+		AND
+		log_output_counts == 1
+		AND
+		allowlisted_counts == 1
+		AND
+		log_disabled_counts == allowlisted_counts
+	);
