e630bbdc291725188bbca4f57a93e485500a72c3
Christian <christian.chavez@nav.no>
2021-09-21 17:48:49 +0200

    Refactor(sql): Simplify sql query.

--- checks/ensure_insecure_users/insecure_users.sql
+++ checks/ensure_insecure_users/insecure_users.sql
@@ -1,15 +1,14 @@
-WITH
-  user_login_shells
-AS (
-  SELECT uid, username, shell
-  FROM users
-  WHERE 
-    username NOT IN ('root', 'sync')
-    AND uid != 1000
-    AND shell NOT LIKE '%/nologin'
-    AND shell != '/bin/false'
-)
 SELECT
-  *
+	uid, username, shell
 FROM
-  user_login_shells;
+	users
+WHERE NOT (
+		shell == '/usr/sbin/nologin'
+	OR
+		shell == '/bin/false'
+) AND NOT (
+		username IN ('root', 'sync')
+	OR
+		-- Default actually logged-in-user's id is 1000 on debian/ubuntu
+		uid == 1000
+);
